7908ccdcba92d63e20b0a4faf3d4e3fec3c5bf20
Sergey Novikov <foxinazombobombo@mail.ru>
2020-07-22 22:15:07 +0700

    Выносит часть логики управления слайдером в отдельный модуль
    Из модуля "slider.js" была вынесена часть логики, отвечающая за управление насыщенностью фильтра, накладываемого на изображение.
    
    Эта логика описана в модуле управления насыщенностью эффекта фильтра — "effectLevel.js".

--- js/slider.js
+++ js/slider.js
@@ -21,26 +21,12 @@ window.slider = (function () {
 
   // ********* DOM-элементы *********
   var IMAGE_PREVIEW_CONTAINER = document.querySelector('.img-upload__preview-container'); // --- Контейнер для превью загруженного изображения в Форму редактирования
-  var IMAGE_PREVIEW = IMAGE_PREVIEW_CONTAINER.querySelector('.img-upload__preview img'); // --- Превью загруженного изображения в Форму редактирования
   var IMAGE_EFFECT_LEVEL = IMAGE_PREVIEW_CONTAINER.querySelector('.img-upload__effect-level'); // --- Группа полей слайдера
-
-  var FIELDSET_OF_FILTERS = document.querySelector('.img-upload__effects'); // --- Контейнер для фильтров, накаладыаемых на изображение
-  var FILTERS = FIELDSET_OF_FILTERS.querySelectorAll('.effects__radio'); // --- Коллекция фильтров, накаладыаемых на изображение
 
   var EFFECT_LEVEL_VALUE = IMAGE_EFFECT_LEVEL.querySelector('.effect-level__value'); // --- <input> для отправки значения насыщенности фильтра на сервер
   var EFFECT_LEVEL_LINE = IMAGE_EFFECT_LEVEL.querySelector('.effect-level__line'); // --- Полоска слайдера
   var EFFECT_LEVEL_PIN = IMAGE_EFFECT_LEVEL.querySelector('.effect-level__pin'); // --- Ручка слайдера
   var EFFECT_LEVEL_DEPTH = IMAGE_EFFECT_LEVEL.querySelector('.effect-level__depth'); // --- Элемент отображения насыщенности фильтра
-
-  // *** Ключи для фильтров ***
-  var Filter = {
-    NONE: 'none',
-    CHROME: 'chrome',
-    SEPIA: 'sepia',
-    MARVIN: 'marvin',
-    PHOBOS: 'phobos',
-    HEAT: 'heat'
-  };
 
 
   /*
@@ -52,68 +38,6 @@ window.slider = (function () {
   EFFECT_LEVEL_VALUE.setAttribute('value', HUNDRED_PERCENT_VALUE);
   EFFECT_LEVEL_PIN.style.left = HUNDRED_PERCENT_VALUE + PERCENT_UNIT;
   EFFECT_LEVEL_DEPTH.style.width = HUNDRED_PERCENT_VALUE + PERCENT_UNIT;
-
-
-  // *** Список функций для переключения фильтров изображения ***
-  var changeFilter = {
-    onNone: function () {
-      IMAGE_PREVIEW.style.filter = '';
-    },
-    onChrome: function (percentOfEffect) {
-      percentOfEffect = percentOfEffect * 0.01;
-      IMAGE_PREVIEW.style.filter = 'grayscale(' + percentOfEffect + ')';
-    },
-    onSepia: function (percentOfEffect) {
-      percentOfEffect = percentOfEffect * 0.01;
-      IMAGE_PREVIEW.style.filter = 'sepia(' + percentOfEffect + ')';
-    },
-    onMarvin: function (percentOfEffect) {
-      IMAGE_PREVIEW.style.filter = 'invert(' + percentOfEffect + '%)';
-    },
-    onPhobos: function (percentOfEffect) {
-      percentOfEffect = percentOfEffect * 0.01 * 3;
-      IMAGE_PREVIEW.style.filter = 'blur(' + percentOfEffect + 'px)';
-    },
-    onHeat: function (percentOfEffect) {
-      percentOfEffect = percentOfEffect * 0.01 * 3;
-      IMAGE_PREVIEW.style.filter = 'brightness(' + percentOfEffect + ')';
-    }
-  };
-
-
-  // *** Функция изменения насыщенности фильтра ***
-  var filterDepthChange = function (percentOfEffect) {
-    var valueOfFilter;
-
-    for (var i = 0; i < FILTERS.length; i++) {
-      if (FILTERS[i].checked) {
-        valueOfFilter = FILTERS[i].value;
-        break;
-      }
-    }
-
-    // --- Условия переключения фильтров ---
-    switch (valueOfFilter) {
-      case Filter.NONE:
-        changeFilter.onNone(percentOfEffect);
-        break;
-      case Filter.CHROME:
-        changeFilter.onChrome(percentOfEffect);
-        break;
-      case Filter.SEPIA:
-        changeFilter.onSepia(percentOfEffect);
-        break;
-      case Filter.MARVIN:
-        changeFilter.onMarvin(percentOfEffect);
-        break;
-      case Filter.PHOBOS:
-        changeFilter.onPhobos(percentOfEffect);
-        break;
-      case Filter.HEAT:
-        changeFilter.onHeat(percentOfEffect);
-        break;
-    }
-  };
 
 
   // *** Обработчик события нажатия кнопки мыши ***
@@ -176,7 +100,7 @@ window.slider = (function () {
 
 
       // --- Изменение насыщенности фильтра ---
-      filterDepthChange(percentOfEffectValue);
+      window.effectLevel.change(percentOfEffectValue);
     };
 
 
