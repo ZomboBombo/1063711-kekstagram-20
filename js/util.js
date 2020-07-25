29b1375eae5be342134cc9133856ae14a3382eed
Sergey Novikov <foxinazombobombo@mail.ru>
2020-07-25 23:10:30 +0700

    Добавляет новые утилиты
    В Утилитарный модуль ( "util.js" ) была добавлена новая утилита: основа системы счисления — константа "NUMBER_SYSTEM".

--- js/util.js
+++ js/util.js
@@ -15,10 +15,10 @@ window.util = (function () {
 
     ZERO_ELEMENT: 0, // --- Нулевой элемент
     DELETE_COUNT: 1, // --- Количество удаляемых элементов ( необходимо для метода ".splice()" )
+    NUMBER_SYSTEM: 10, // --- Основа системы счисления
 
 
     // ********* ФУНКЦИИ *********
-
     // --- Рандомайзер значений ---
     getRandomNumber: function (min, max) {
       return Math.floor(min + Math.random() * max);
