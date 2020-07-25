0caae04ece08d9c847718046a3a8e5888f1fb3fb
Sergey Novikov <foxinazombobombo@mail.ru>
2020-07-25 23:07:37 +0700

    Добавляет логику уведомления о неправильном типе файла
    В модуль загрузки изображения пользователя ( "userImageLoad.js" ) была добавлена логика уведомления об ошибке в выборе файла.
    
    Ошибка возникает, если пользователь пытается выбрать файл неподходящего формата.

--- js/userImageLoad.js
+++ js/userImageLoad.js
@@ -18,9 +18,19 @@ window.userImageLoad = (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   // ********* DOM-элементы *********
+  var MAIN = document.querySelector('main');
+  var ERROR_WINDOW = document.querySelector('#error').content.querySelector('.error');
+
   var UPLOAD_SECTION = document.querySelector('.img-upload');
   var FILE_CHOOSER = UPLOAD_SECTION.querySelector('#upload-file');
   var IMAGE_PREVIEW = UPLOAD_SECTION.querySelector('.img-upload__preview img');
+
+
+  // *** Переменные, содержащие элементы модального окна Ошибки ***
+  var errorModal = ERROR_WINDOW.cloneNode(true);
+  var errorModalInner = errorModal.querySelector('.error__inner');
+  var errorModalTitle = errorModal.querySelector('.error__title');
+  var errorCloseButton = errorModalInner.querySelector('.error__button');
 
 
   /*
@@ -28,8 +38,52 @@ window.userImageLoad = (function () {
   --------------------------------- ОСНОВНАЯ ЛОГИКА --------------------------------
   ----------------------------------------------------------------------------------
   */
-  // *** Обработчик события изменения состояния поля загрузки файла ***
-  FILE_CHOOSER.addEventListener('change', function () {
+  // ************* Функции обработки событий закрытия модального окна *************
+  var onErrorEscPress = function (evt) {
+    if (evt.key === window.util.ESC) {
+      evt.preventDefault();
+      errorDialog.close();
+    }
+  };
+
+  var onClickOutError = function (evt) {
+    if (evt.target !== errorModalInner) {
+      errorDialog.close();
+    }
+  };
+
+  var onErrorModalClose = function (evt) {
+    evt.preventDefault();
+    errorDialog.close();
+  };
+
+
+  // *** Модальное окно ОШИБКИ ***
+  var errorDialog = {
+    open: function () {
+      errorModalTitle.textContent = 'Неверный тип файла! Пожалуйста, выберите другой файл в формате GIF, JPG, JPEG или PNG.';
+      errorModalTitle.style.lineHeight = '45px';
+      MAIN.appendChild(errorModal);
+
+      // === ДОБАВЛЕНИЕ обработчиков событий закрытия окна ===
+      errorCloseButton.addEventListener('click', onErrorModalClose);
+      document.addEventListener('click', onClickOutError);
+      document.addEventListener('keydown', onErrorEscPress);
+    },
+
+    close: function () {
+      MAIN.removeChild(errorModal);
+
+      // === УДАЛЕНИЕ обработчиков событий закрытия окна ===
+      errorCloseButton.removeEventListener('click', onErrorModalClose);
+      document.removeEventListener('click', onClickOutError);
+      document.removeEventListener('keydown', onErrorEscPress);
+    }
+  };
+
+
+  // *** Функция для обработки события выбора файла ***
+  var onChoosingFile = function () {
     var file = FILE_CHOOSER.files[FIRST_FILE]; // --- Первый элемент из массива файлов
     var fileName = file.name.toLowerCase(); // --- Имя выбранного файла
 
@@ -37,7 +91,6 @@ window.userImageLoad = (function () {
     var matches = FILE_TYPES.some(function (element) {
       return fileName.endsWith(element);
     });
-
 
     /*
       Условие: если тип выбранного файла совпадает
@@ -51,11 +104,18 @@ window.userImageLoad = (function () {
       // --- Обработчик события завершения чтения файла ---
       reader.addEventListener('load', function () {
         IMAGE_PREVIEW.src = reader.result;
+        window.imageEditingForm.open();
       });
 
       // --- Чтение выбранного файла ---
       reader.readAsDataURL(file);
+    } else {
+      errorDialog.open();
     }
-  });
+  };
+
+
+  // *** Обработчик события изменения состояния поля загрузки файла ***
+  FILE_CHOOSER.addEventListener('change', onChoosingFile);
 
 })();
