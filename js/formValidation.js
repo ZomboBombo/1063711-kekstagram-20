3a33b49fb064d45bd813a7b676842ec5925462a6
Sergey Novikov <foxinazombobombo@mail.ru>
2020-07-26 13:30:06 +0700

    Дробит длинную функцию на несколько маленьких
    В модуле валидации полей Формы ( "formValidation.js" ) громоздкая функция "checkHashtagField()" подверглась дроблению: часть основной логики была вынесена в отдельную функцию "showHashtagValidationMessage()".
    
    Похожему дроблению подверглась и функция "checkTextArea()": часть основной логики была вынесена в функцию "showDescriptionValidationMessage()".

--- js/formValidation.js
+++ js/formValidation.js
@@ -14,6 +14,7 @@ window.formValidation = (function () {
   ----------------------------------------------------------------------------------
   */
   // ********* КОНСТАНТЫ *********
+  var REG_EXP_FOR_HASHTAG = /^#[\wа-яА-Я]*$/; // --- Паттерн для валидации Хештегов
   var SEPARATOR = ' '; // --- Символ «пробел» — разделитель для метода "split()"
   var MAX_HASHTAG_LENGTH = 20; // --- Максимальная длина хештега
   var MAX_HASHTAG_COUNT = 5; // --- Максимальное количество хештегов
@@ -45,65 +46,92 @@ window.formValidation = (function () {
   };
 
 
-  return {
-    checkHashtagField: function () {
-      var regExpForHashtag = /^#[\wа-яА-Я]*$/; // --- Паттерн для валидации Хештегов
-      var notEmptyHashtagField = fieldForHashtags.value !== window.util.ATTRIBUTE_EMPTY_VALUE; // --- НЕ пустое поле для хештегов
+  // *** Функция показа валидационного сообщения для поля хештегов ***
+  var showHashtagValidationMessage = function (arrayOfHashtags, notEmptyField) {
+    // *** Цикл для валидации Хештегов ***
+    for (var i = 0; i < arrayOfHashtags.length; i++) {
+      var matchCount = 0; // --- Количество повторяющихся хештегов
 
-      // --- Массив — набор хештегов из поля ввода ---
-      var hashtags = fieldForHashtags.value.split(SEPARATOR);
-
-      // *** Цикл для валидации Хештегов ***
-      for (var i = 0; i < hashtags.length; i++) {
-        var matchCount = 0; // --- Количество повторяющихся хештегов
-
-        // --- Проверка на повторяющиеся хештеги в массиве ---
-        for (var j = i + 1; j < hashtags.length; j++) {
-          if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase()) {
-            matchCount++;
-          }
-        }
-
-
-        // *** Справочник ОШИБОК валидации ***
-        var error = {
-          patternMismatch: !regExpForHashtag.test(hashtags[i]),
-          loneHash: hashtags[i] === '#',
-          tooLongHashtag: hashtags[i].length >= MAX_HASHTAG_LENGTH,
-          tooManyHashtags: hashtags.length > MAX_HASHTAG_COUNT,
-          duplicateHashtags: matchCount > 0
-        };
-
-        // *** Справочник СООБЩЕНИЙ об ошибках валидации ***
-        var message = {
-          patternMismatch: 'Ошибка! Хештег «' + hashtags[i] + '» не соответствует паттерну ввода! Хештег не может содержать пробелы, специальные символы (типа «@, $, #»), символы пунктуации (тире, дефис, запятая и т.д.), а также эмодзи.',
-          loneHash: 'Ошибка! Хештег «' + hashtags[i] + '» не может состоять только из "решётки"!',
-          tooLongHashtag: 'Ошибка! Длина хештега «' + hashtags[i] + '» превышает 20 символов!',
-          tooManyHashtags: 'Ошибка! Нельзя добавить больше 5-ти хештегов! Количество хештегов сейчас: ' + hashtags.length,
-          duplicateHashtags: 'Ошибка! Набор не может содержать несколько одинаковых хештегов!'
-        };
-
-
-        // --- Правила валидации набора хештегов ---
-        if (error.patternMismatch && notEmptyHashtagField) {
-          validationError.add(fieldForHashtags, message.patternMismatch);
-          break;
-        } else if (error.tooManyHashtags) {
-          validationError.add(fieldForHashtags, message.tooManyHashtags);
-          break;
-        } else if (error.loneHash) {
-          validationError.add(fieldForHashtags, message.loneHash);
-          break;
-        } else if (error.tooLongHashtag) {
-          validationError.add(fieldForHashtags, message.tooLongHashtag);
-          break;
-        } else if (error.duplicateHashtags) {
-          validationError.add(fieldForHashtags, message.duplicateHashtags);
-          break;
-        } else {
-          validationError.remove(fieldForHashtags);
+      // --- Проверка на повторяющиеся хештеги в массиве ---
+      for (var j = i + 1; j < arrayOfHashtags.length; j++) {
+        if (arrayOfHashtags[i].toLowerCase() === arrayOfHashtags[j].toLowerCase()) {
+          matchCount++;
         }
       }
+
+      // *** Справочник ОШИБОК валидации ***
+      var error = {
+        patternMismatch: !REG_EXP_FOR_HASHTAG.test(arrayOfHashtags[i]),
+        loneHash: arrayOfHashtags[i] === '#',
+        tooLongHashtag: arrayOfHashtags[i].length >= MAX_HASHTAG_LENGTH,
+        tooManyHashtags: arrayOfHashtags.length > MAX_HASHTAG_COUNT,
+        duplicateHashtags: matchCount > 0
+      };
+
+      // *** Справочник СООБЩЕНИЙ об ошибках валидации ***
+      var message = {
+        patternMismatch: 'Ошибка! Хештег «' + arrayOfHashtags[i] + '» не соответствует паттерну ввода! Хештег не может содержать пробелы, специальные символы (типа «@, $, #»), символы пунктуации (тире, дефис, запятая и т.д.), а также эмодзи.',
+        loneHash: 'Ошибка! Хештег «' + arrayOfHashtags[i] + '» не может состоять только из "решётки"!',
+        tooLongHashtag: 'Ошибка! Длина хештега «' + arrayOfHashtags[i] + '» превышает 20 символов!',
+        tooManyHashtags: 'Ошибка! Нельзя добавить больше 5-ти хештегов! Количество хештегов сейчас: ' + arrayOfHashtags.length,
+        duplicateHashtags: 'Ошибка! Набор не может содержать несколько одинаковых хештегов!'
+      };
+
+
+      // --- Правила валидации набора хештегов ---
+      if (error.patternMismatch && notEmptyField) {
+        validationError.add(fieldForHashtags, message.patternMismatch);
+        break;
+      } else if (error.tooManyHashtags) {
+        validationError.add(fieldForHashtags, message.tooManyHashtags);
+        break;
+      } else if (error.loneHash) {
+        validationError.add(fieldForHashtags, message.loneHash);
+        break;
+      } else if (error.tooLongHashtag) {
+        validationError.add(fieldForHashtags, message.tooLongHashtag);
+        break;
+      } else if (error.duplicateHashtags) {
+        validationError.add(fieldForHashtags, message.duplicateHashtags);
+        break;
+      } else {
+        validationError.remove(fieldForHashtags);
+      }
+    }
+  };
+
+
+  // *** Функция показа валидационного сообщения для описания фотографии ***
+  var showDescriptionValidationMessage = function () {
+    var descriptionError = {
+      tooManySymbols: fieldForDescription.value.length > MAX_DESCRIPTION_LENGTH
+    };
+
+    var descriptionErrorMessage = {
+      tooManySymbols: 'Количество символов описания фотографии не должно превышать ' + MAX_DESCRIPTION_LENGTH + ' символов! Количество символов сейчас: ' + fieldForDescription.value.length
+    };
+
+
+    // --- Правила валидации описания к фотографии ---
+    switch (true) {
+      // --- Слишком длинное описание (количество символов не должно превышать 140) ---
+      case descriptionError.tooManySymbols:
+        return validationError.add(fieldForDescription, descriptionErrorMessage.tooManySymbols);
+
+      default:
+        return validationError.remove(fieldForDescription);
+    }
+  };
+
+
+  return {
+    checkHashtagField: function () {
+      var notEmptyHashtagField = fieldForHashtags.value !== window.util.ATTRIBUTE_EMPTY_VALUE; // --- НЕ пустое поле для хештегов
+      var hashtags = fieldForHashtags.value.split(SEPARATOR); // --- Массив — набор хештегов из поля ввода
+
+      // *** Уведомление о проверке ***
+      showHashtagValidationMessage(hashtags, notEmptyHashtagField);
+
 
       /*
         ______________________________________
@@ -116,42 +144,26 @@ window.formValidation = (function () {
     },
 
     checkTextArea: function () {
-      var descriptionError = {
-        tooManySymbols: fieldForDescription.value.length > MAX_DESCRIPTION_LENGTH
-      };
-
-      var descriptionErrorMessage = {
-        tooManySymbols: 'Количество символов описания фотографии не должно превышать ' + MAX_DESCRIPTION_LENGTH + ' символов! Количество символов сейчас: ' + fieldForDescription.value.length
-      };
+      // *** Уведомление о проверке ***
+      showDescriptionValidationMessage();
 
 
-      // --- Правила валидации описания к фотографии ---
-      switch (true) {
-        // --- Слишком длинное описание (количество символов не должно превышать 140) ---
-        case descriptionError.tooManySymbols:
-          return validationError.add(fieldForDescription, descriptionErrorMessage.tooManySymbols);
+      /*
+      ___________________________________________________________________
 
-        default:
-          return validationError.remove(fieldForDescription);
-      }
+        Текст, который должен быть больше одной сотни и четырёх десятков
+        да ни одной единицы, чтобы я смог проверить, как работает
+        ( и работает ли вообще ) моя кастомная валидация.
+
+        --- ---
+
+        This is the text that i have been modified to do something
+        and it is wron and incorrect multiple words system,
+        but i don't have so many time to learn my English.
+      ___________________________________________________________________
+
+      */
     },
-
-    /*
-    ___________________________________________________________________
-
-      Текст, который должен быть больше одной сотни и четырёх десятков
-      да ни одной единицы, чтобы я смог проверить, как работает
-      ( и работает ли вообще ) моя кастомная валидация.
-
-      --- ---
-
-      This is the text that i have been modified to do something
-      and it is wron and incorrect multiple words system,
-      but i don't have so many time to learn my English.
-    ___________________________________________________________________
-
-    */
-
 
     errorReport: validationError
   };
